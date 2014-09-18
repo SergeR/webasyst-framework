93733d633a3ddd8aec3b3a00748756d06c3ca8fe
Serge Rodovnichenko <sergerod@gmail.com>
2014-09-18 21:32:04 +0400

    Docblocks added

--- wa-system/cache/adapters/waMemcachedCacheAdapter.class.php
+++ wa-system/cache/adapters/waMemcachedCacheAdapter.class.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * This file is part of Webasyst framework.
+ *
+ * Licensed under the terms of the GNU Lesser General Public License (LGPL).
+ * http://www.webasyst.com/framework/license/
+ *
+ * @link http://www.webasyst.com/
+ * @author Webasyst LLC
+ * @copyright 2011 Webasyst LLC
+ * @package wa-system/Cache/Adapter
+ * @license http://www.webasyst.com/framework/license/ LGPL
+ */
 
 class waMemcachedCacheAdapter extends waCacheAdapter
 {
